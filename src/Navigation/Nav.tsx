572263a44affbfad52a7917ded24d5a166eecc76
Jules Dieu donné - G3 <mdjulesdeveloper23@gmail.com>
2025-12-10 18:32:40 +0000

    Adding the Navigation

---
+++ src/Navigation/Nav.tsx
@@ -0,0 +1,39 @@
+import { FiHeart } from "react-icons/fi";
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
+
+import "./Nav.css";
+type Props = {
+  query: string;
+  handleInputChange: (e: any) => void;
+};
+
+const Nav = ({ query, handleInputChange }: Props) => {
+  return (
+    <nav>
+      <div className="nav-container">
+        <input
+          value={query}
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Enter your search shoes."
+          className="search-input"
+        />
+      </div>
+      <div className="profile-container">
+        <a href="#">
+          <FiHeart className="nav-icons" />
+        </a>
+
+        <a href="#">
+          <AiOutlineShoppingCart className="nav-icons" />
+        </a>
+
+        <a href="#">
+          <AiOutlineUserAdd className="nav-icons" />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
